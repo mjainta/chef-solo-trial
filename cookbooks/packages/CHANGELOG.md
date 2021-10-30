d9546d644e4b57bf8998c683f7356a47ad698653
Martin Jainta <martin.jainta@gmail.com>
2021-10-30 08:59:45 +0200

    ✨ Add packages cookbook

---
+++ cookbooks/packages/CHANGELOG.md
@@ -0,0 +1,10 @@
+# packages CHANGELOG
+
+This file is used to list changes made in each version of the packages cookbook.
+
+## 0.1.0
+
+Initial release.
+
+- change 0
+- change 1
---
+++ cookbooks/packages/recipes/default.rb
@@ -0,0 +1,46 @@
+#
+# Cookbook:: packages
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+repos = [
+  'ppa:shutter/ppa',
+  'ppa:mikhailnov/pulseeffects',
+]
+
+packages = [
+  'tilix',
+  'docker-ce',
+  'git',
+  'tilix',
+  # 'fzf', Does not exist on 18.04
+  'shutter',
+  'tig',
+  'speedcrunch',
+  'xtightvncviewer',
+  'pulseeffects',
+]
+
+snaps = [
+  'code',
+  'spotify',
+]
+
+repos.each { |repo_uri|
+  apt_repository 'add_repo' do
+    uri repo_uri
+  end
+}
+
+apt_package packages do
+  action :install
+end
+
+# This does not work, fails with 'wrong number of arguments (given 1, expected 2)'
+# snap_package 'install_snaps' do
+#   action :install
+#   package_name snaps
+#   options ['classic']
+# end
+
+execute 'snap install spotify code'
